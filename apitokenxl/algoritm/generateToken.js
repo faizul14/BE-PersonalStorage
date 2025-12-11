13b328744aadd0f914a676c4ae709470b84247ad
Faezol <fpcyber61@gmail.com>
2025-12-11 15:58:47 +0800

    feat: create endpoint custom token

--- apitokenxl/algoritm/generateToken.js
+++ apitokenxl/algoritm/generateToken.js
@@ -4,4 +4,10 @@ function generateTokenXL(length) {
     return crypto.randomBytes(Math.ceil(length / 2)).toString('hex').slice(0, length);
 }
 
-module.exports = { generateTokenXL }
+function generateTokenCustomXL(uixToken, length) {
+    const generateToken = crypto.randomBytes(Math.ceil(length / 2)).toString('hex').slice(0, length);
+    const uinxTokenFix = uixToken.replaceAll(" ", "")
+    return `${uinxTokenFix}-${generateToken}`
+}
+
+module.exports = { generateTokenXL, generateTokenCustomXL }
